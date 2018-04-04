edca1b7dbedcf7881a747d02add7aabc817ee3a7
Dandi <rsmohamad@ust.hk>
2018-04-04 15:04:58 +0800

    add stuff

--- Makefile
+++ Makefile
@@ -1,9 +1,24 @@
 CC=clang
+SRCS=$(wildcard *.c)
+OBJS=$(patsubst %.c, %.o, $(SRCS))
+CFLAGS=
+
+%.o: %.c
+	clang-format -i -style=google $^
+	$(CC) -c $< -o $@ $(CFLAGS)
 
 default: server
 
-server: main.c
-	$(CC) $< -o $@ -pthread
+server: $(OBJS) libz.a
+	$(CC) $^ -o $@ $(CFLAGS) -pthread
 
 clean:
-	rm -r server
+	rm -f server *.o *.a *.zip
+
+libz.a:
+	@cd zlib && ./configure
+	$(MAKE) -C zlib/ static && cp zlib/libz.a ./
+	$(MAKE) -C zlib/ clean
+
+zip:
+	git archive --format=zip --prefix=comp4621/ --output=comp4621.zip HEAD
--- main.c
+++ main.c
@@ -1,64 +1,69 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <pthread.h>
 
-#define MAXLINE 1024
+#define MAXLINE 2048
 
-void *serveFile(void *sock){
-    int sockfd = *(int*)sock;
-    char txbuf[MAXLINE] = {0};
-    sprintf(txbuf, "Hello world\n");
-    write(sockfd, txbuf, strlen(txbuf));
-    close(sockfd);
-    return 0;
+void *serveFile(void *sock) {
+  int sockfd = *(int *)sock;
+  char txbuf[MAXLINE] = {0};
+  sprintf(txbuf, "Hello world\n");
+  write(sockfd, txbuf, strlen(txbuf));
+  close(sockfd);
+  usleep(1000);
+  return 0;
 }
 
-int main(int argc, char**argv) {
-    int listenfd, sockfd;
-    struct sockaddr_in servaddr, cliaddr;
-    socklen_t len = sizeof(struct sockaddr_in);
+int main(int argc, char **argv) {
+  int listenfd, sockfd;
+  struct sockaddr_in servaddr, cliaddr;
+  socklen_t len = sizeof(struct sockaddr_in);
 
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(80);
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = INADDR_ANY;
+  servaddr.sin_port = htons(80);
 
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Error: cannot open a socket\n");
-        return 1;
+  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    fprintf(stderr, "Error: cannot open a socket\n");
+    return 1;
+  }
+
+  if (bind(listenfd, (struct sockaddr *)&servaddr, len) < 0) {
+    fprintf(stderr, "Error: cannot bind to port 80\n");
+    return 1;
+  }
+
+  if (listen(listenfd, 5) < 0) {
+    fprintf(stderr, "Error: cannot listen on port 80\n");
+    return 1;
+  }
+
+  printf("Listening on port 80 ...\n");
+
+  while (1) {
+    if ((sockfd = accept(listenfd, (struct sockaddr *)&cliaddr, &len)) < 0) {
+      fprintf(stderr, "Error: cannot accept incoming request\n");
+      continue;
     }
 
-    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        printf("Error: cannot bind to port 80\n");
-        return 1;
+    char ip_str[INET_ADDRSTRLEN] = {0};
+    inet_ntop(AF_INET, &(cliaddr.sin_addr), ip_str, INET_ADDRSTRLEN);
+    printf("Incoming connection from %s\n", ip_str);
+
+    pthread_t tid;
+    if (pthread_create(&tid, NULL, serveFile, (void *)&sockfd) < 0) {
+      fprintf(stderr, "Error: cannot create thread for %s\n", ip_str);
+      close(sockfd);
     }
 
-    if (listen(listenfd, 5) < 0) {
-        printf("Error: cannot listen to port 80\n");
-        return 1;
-    }
+    usleep(1000);
+  }
 
-    while (1) {
-        if ((sockfd = accept(listenfd, (struct sockaddr *)&cliaddr, &len)) < 0) {
-            printf("Error: cannot accept incoming TCP request\n");
-            return 1;
-        }
-
-        char ip_str[INET_ADDRSTRLEN] = {0};
-        inet_ntop(AF_INET, &(cliaddr.sin_addr), ip_str, INET_ADDRSTRLEN);
-        printf("Incoming connection from %s\n", ip_str);
-        
-        pthread_t tid;
-        pthread_create(&tid, NULL, serveFile, (void *)&sockfd);
-
-    }
-
-
-    return 0;
+  return 0;
 }
